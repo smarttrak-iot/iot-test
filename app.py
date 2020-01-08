971eb1be9b7e6fb7d22959ce303f4e73d2c3f4bc
Bhagawan <bhagawan@smarttrak.co>
2020-01-08 11:12:51 -0800

    Initial commit, REST API testing

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+api = Api(app)
+
+from models.testmodel import db,DevModel
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+#devices = []
+
+class Dev(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('power',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+    parser.add_argument('voltage',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+    parser.add_argument('current',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+        )
+
+
+    def get(self,devId):
+        device =  DevModel.query.filter_by(devId=devId).first()
+        if device:
+            return device.json()
+        return {'message': 'device not found'}, 404
+
+
+    def post(self,devId):
+        if DevModel.query.filter_by(devId=devId).first():
+            return {'message': "A device with id '{}' already exists.".format(devId)}
+        data = Dev.parser.parse_args()
+        #device = {'devId':devId,'power':data['power'],'voltage':data['voltage'],'current':data['current']}
+        device = DevModel(devId,data['power'],data['voltage'],data['current'])
+
+        #devices.append(device)
+        #return device
+        try:
+            db.session.add(device)
+            db.session.commit()
+        except:
+            return {"message": "An error occurred inserting the device."}, 500
+
+        return device.json(), 201
+
+    def put(self,devId):
+        data = Dev.parser.parse_args()
+
+        #device = next(filter(lambda x: x['devId'] == devId,devices), None)
+        device = DevModel.query.filter_by(devId=devId).first()
+
+        if device is None:
+            device = DevModel(devId,data['power'],data['voltage'],data['current'])
+        else:
+            device.power = data['power']
+            device.voltage = data['voltage']
+            device.current = data['current']
+
+            db.session.add(device)
+            db.session.commit()
+
+        return device.json()
+
+
+class DeviceList(Resource):
+    def get(self):
+        return {'devices': list(map(lambda x: x.json(), DevModel.query.all()))}
+        #return{'devices':devices}
+
+api.add_resource(Dev,'/device/<string:devId>')
+api.add_resource(DeviceList,'/devices')
+
+
+db.init_app(app)
+app.run(port=5000, debug=True)
---
+++ models/testmodel.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+class DevModel(db.Model):
+    devId = db.Column(db.String(20), primary_key=True)
+    power = db.Column(db.Float(precision=2))
+    voltage = db.Column(db.Float(precision=2))
+    current = db.Column(db.Float(precision=2))
+    def __init__(self, devId,power,voltage,current):
+        self.devId = devId
+        self.power = power
+        self.voltage = voltage
+        self.current = current
+
+    def json(self):
+        return {'devId': self.devId,'power': self.power,'voltage': self.voltage,'current': self.current}
